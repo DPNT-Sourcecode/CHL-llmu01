916010b323e311d33b93405546664401b0e36543
sbx_user1051 <sbx_user1051@169.254.55.33>
2019-11-02 10:18:12 +0000

    Sat Nov 02 10:18:12 UTC 2019

--- src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -1,5 +1,19 @@
+package befaster.solutions.CHL;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class CheckliteSolutionTest {
+
+    private CheckliteSolution solution = new CheckliteSolution();
+
+    @Test
+    public void handleEmptySkus() {
+        assertThat(solution.checklite(""), is(equalTo(-1)));
+    }
 
 }
