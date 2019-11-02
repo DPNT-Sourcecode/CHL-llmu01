8f60e31c66687764ebee489b0566b640392a85a4
sbx_user1051 <sbx_user1051@169.254.55.33>
2019-11-02 12:03:02 +0000

    Sat Nov 02 12:03:02 UTC 2019

--- src/main/java/befaster/solutions/CHL/Item.java
+++ src/main/java/befaster/solutions/CHL/Item.java
@@ -1,4 +1,30 @@
 package befaster.solutions.CHL;
 
+import java.util.Optional;
+
 public class Item {
+
+    private Character name;
+    private int basePrice;
+
+    private Optional<SpecialOffer> specialOffer;
+
+    public Item(Character name, int basePrice, Optional<SpecialOffer> specialOffer) {
+        this.name = name;
+        this.basePrice = basePrice;
+        this.specialOffer = specialOffer;
+    }
+
+    public Character getName() {
+        return name;
+    }
+
+    public int getBasePrice() {
+        return basePrice;
+    }
+
+    public Optional<SpecialOffer> getSpecialOffer() {
+        return specialOffer;
+    }
 }
+
---
+++ src/main/java/befaster/solutions/CHL/SpecialOffer.java
@@ -0,0 +1,7 @@
+package befaster.solutions.CHL;
+
+public class SpecialOffer {
+
+    private int offerQuantity;
+    private int offerPrice;
+}
